cf820f3a09b15b6cfc126e3c840609e59f2f35ce
Alexandra Fren <35177685+alexandrafren@users.noreply.github.com>
2018-05-23 17:09:01 -0400

    Create mycode.js

---
+++ mycode.js
@@ -0,0 +1,20 @@
+//Note: this code does not run as is
+
+function factorialize(num) {
+ var total;
+  while(i = num; i >= 1; i--){
+   if (i === 2){
+     total = 2;
+   }
+   else if (i > 2){
+     total = total + (i)(i-1);
+   }
+   else{
+     console.log('Error');
+   }
+ }
+  return num;
+}
+
+factorialize(5);
+console.log(total);
